99f98d1cb9df88122e7ced23816b466066dc0c36
Vidit Prajapati <vidit.msit@gmail.com>
2024-03-24 13:57:53 +0530

    Portal hook added

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,7 @@
-import { forwardRef ,useImperativeHandle, useRef } from "react"
+import { forwardRef ,useImperativeHandle, useRef } from "react";
+import {createPortal} from 'react-dom';
+
+
  const ResultModal = forwardRef(function ResultModal(
   {targetTime,remainingTime,onReset},ref) {
   const dialog =useRef();
@@ -19,7 +22,7 @@ import { forwardRef ,useImperativeHandle, useRef } from "react"
     }
   })
   
-  return(  <dialog ref={dialog} className="result-modal"  onClose={onReset} >
+  return createPortal(  <dialog ref={dialog} className="result-modal"  onClose={onReset} >
     {userLost && <h2>Your LOST </h2>}
     {!userLost && <h2>Your Score : {score} </h2>}
     <p>Target Time was <strong>{targetTime}</strong></p>
@@ -30,6 +33,6 @@ import { forwardRef ,useImperativeHandle, useRef } from "react"
     <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
     </form>
-  </dialog>);
+  </dialog>, document.getElementById('modal'));
 })
 export default ResultModal;
